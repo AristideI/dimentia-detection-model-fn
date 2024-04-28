80976c25dcfcba6a3a1b4bbbffaef462f8040e8c
aristidei <a.isingizwe@alustudent.com>
2024-04-28 12:06:09 +0200

    add patient and doctor

--- src/utils/addPatient.tsx
+++ src/utils/addPatient.tsx
@@ -1,15 +1,18 @@
-import { UserReqDto } from "../types/interfaces";
+import { Except } from "type-fest";
+import { PatientDto } from "../types/interfaces";
 import { apiUrl } from "./apiUrl";
 import axios from "axios";
 
-export default async function addDoctor(doctorData: UserReqDto) {
-  const url = `${apiUrl}doctor`;
+export default async function addPatient(
+  patientData: Except<PatientDto, "id">
+) {
+  const url = `${apiUrl}patient`;
   const token = localStorage.getItem("token");
 
   if (!token) {
     throw new Error("Token not found");
   }
-  const newUser = await axios.post(url, doctorData, {
+  const newUser = await axios.post(url, patientData, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
