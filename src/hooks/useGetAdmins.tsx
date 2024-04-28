d4c44feb7bc0d6d94d4c10d49bdd400069413d05
aristidei <a.isingizwe@alustudent.com>
2024-04-28 12:49:21 +0200

    hooks

---
+++ src/hooks/useGetAdmins.tsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { apiUrl } from "../utils/apiUrl";
+import { UserResDto } from "../types/interfaces";
+import { useEffect, useState } from "react";
+
+export function useGetAdmins() {
+  const [admins, setAdmins] = useState<UserResDto[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchAdmins = async () => {
+      try {
+        const allAdmins = await getAdmins();
+        setAdmins(allAdmins);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+      }
+    };
+    fetchAdmins();
+  }, []);
+
+  return { admins, loading, error };
+}
+
+async function getAdmins() {
+  const url = `${apiUrl}admin-user`;
+  const token = localStorage.getItem("token");
+  const admins = await axios.get<UserResDto[]>(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return admins.data;
+}
---
+++ src/hooks/useGetDoctor.tsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { apiUrl } from "../utils/apiUrl";
+import { UserResDto } from "../types/interfaces";
+import { useEffect, useState } from "react";
+
+export function useGetDoctors() {
+  const [doctors, setDoctors] = useState<UserResDto[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchAdmins = async () => {
+      try {
+        const allAdmins = await getAdmins();
+        setDoctors(allAdmins);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+      }
+    };
+    fetchAdmins();
+  }, []);
+
+  return { doctors, loading, error };
+}
+
+async function getAdmins() {
+  const token = localStorage.getItem("token");
+  const url = `${apiUrl}doctor`;
+  const admins = await axios.get<UserResDto[]>(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return admins.data;
+}
