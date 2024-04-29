e254e1a43ae0488268ecfac30668d21302bb895a
aristidei <a.isingizwe@alustudent.com>
2024-04-29 18:45:59 +0200

    add admins and doctors

--- src/main.tsx
+++ src/main.tsx
@@ -3,25 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import AuthContextProvider from "./context/AuthContext.tsx";
-import { Bounce, ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthContextProvider>
       <App />
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-        transition={Bounce}
-      />
     </AuthContextProvider>
   </React.StrictMode>
 );
