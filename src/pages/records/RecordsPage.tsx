1302ae6c0cb5e9703b87c8b506c5c6b17ef5073c
yves137 <oneg2981@gmail.com>
2024-05-05 10:41:53 +0200

    refactor: add test for doctor only:

--- src/pages/records/RecordsPage.tsx
+++ src/pages/records/RecordsPage.tsx
@@ -2,20 +2,25 @@ import { useParams } from "react-router";
 import PatientRecordsTable from "../../components/tables/PatientRecordsTable";
 import { useGetPatientRecords } from "../../hooks/useGetPatientRecords";
 import { Link } from "react-router-dom";
+import { UserResDto } from "../../types/interfaces";
 
 export default function RecordsPage() {
   const patientId = useParams().id;
   const { patientRecords } = useGetPatientRecords(patientId || "");
+  const user = JSON.parse(localStorage.getItem("user") || "{}") as UserResDto;
+  const isAdmin = user.isAdmin;
   return (
     <article>
       <section className="flex justify-between items-center px-10">
         <h1 className="text-3xl font-semibold text-dark">Patient Records</h1>
-        <Link
-          to={`/patients/${patientId}/test`}
-          className="font-bold text-primary-100 bg-primary-400 px-8 py-2 rounded-md"
-        >
-          Add Medical Test
-        </Link>
+        {!isAdmin && (
+          <Link
+            to={`/patients/${patientId}/test`}
+            className="font-bold text-primary-100 bg-primary-400 px-8 py-2 rounded-md"
+          >
+            Add Medical Test
+          </Link>
+        )}
       </section>
       {patientRecords && (
         <PatientRecordsTable patientRecords={patientRecords.records} />
